f4d257402e4e2d2fa2f49ed14a24d4262bfd05c6
Murokarg <edwin.rene.rodriguez.ortiz@gmail.com>
2026-01-17 09:07:29 -0600

    week1 arrays exercise

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,16 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-
-        return []; // replace this return statement with your own
+        // Step 1: Create an array of size 'Length'
+        double[] result = new double[length];
+        // Step 2: Iterate from 0 to count-1 (fill out with the multiples)
+        for (int i = 0; i < length; i++)
+        {
+            // Step 3: Calculate the multiple: number * (i+1)
+            result[i] = number * (i + 1);
+        }
+        // Step 3: Return the array 
+        return result;
     }
 
     /// <summary>
@@ -29,5 +37,15 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Step 1: Extract the last 'amount' elements
+        var lastElements = data.GetRange(data.Count - amount, amount);
+
+        // Step 2: Eliminate those elements from the end of the original list
+        data.RemoveRange(data.Count - amount, amount);
+        
+        // Step 3: Insert the elements at the beginning of the original list
+        data.InsertRange(0, lastElements);
+
     }
 }
